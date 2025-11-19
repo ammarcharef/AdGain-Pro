ea0be4153edbd6ff5b775685e754501ab557d4d4
Ammar <ammarcharef2006@gmail.com>
2025-11-20 00:18:04 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,3 +1,4 @@
+const ADMIN_CHAT_ID = '2140385904'; // ضع رقمك هنا بدقة (بدون فواصل)
 const TelegramBot = require('node-telegram-bot-api');
 const mongoose = require('mongoose');
 const User = require('./models/User');
