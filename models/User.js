4d4b3fcd9c0066f54c7f63f593d8aebc9d1a9657
Ammar <ammarcharef2006@gmail.com>
2025-11-20 14:54:49 +0100

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,15 +1,16 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-    username: { type: String, unique: true }, // نحفظ فيه معرف تليجرام Tg_12345
-    telegramId: { type: String, required: true, unique: true }, // المعرف الرقمي الصافي
+    username: { type: String, unique: true },
+    telegramId: { type: String, required: true, unique: true }, // حاسم لربط البوت
     firstName: { type: String },
     balance: { type: Number, default: 0 },
     xp: { type: Number, default: 0 },
     level: { type: Number, default: 1 },
     referralCode: { type: String, unique: true },
     referredBy: { type: String, default: null },
-    withdrawalAccount: { type: String, default: null }, // آخر حساب سحب تم استخدامه
+    withdrawalAccount: { type: String, default: null }, 
+    isAdmin: { type: Boolean, default: false }, // صلاحية المدير (يتم تعديلها يدوياً)
     joinedAt: { type: Date, default: Date.now }
 });
 
