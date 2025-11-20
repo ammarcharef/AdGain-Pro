582d21909d8150ce7d433710a872f1a58ec605a9
Ammar <ammarcharef2006@gmail.com>
2025-11-20 14:55:20 +0100

    Update Withdrawal.js

--- models/Withdrawal.js
+++ models/Withdrawal.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const WithdrawalSchema = new mongoose.Schema({
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    amount: { type: Number, required: true },
+    amount: { type: Number, required: true, min: 500 },
     paymentMethod: { type: String, required: true },
     accountDetails: { type: String, required: true },
     status: { type: String, enum: ['Pending', 'Paid', 'Rejected'], default: 'Pending' },
