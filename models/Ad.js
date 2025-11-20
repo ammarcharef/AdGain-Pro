3be69995c006a282af8878c8bf9e5dc5da8ae411
Ammar <ammarcharef2006@gmail.com>
2025-11-20 01:45:02 +0100

    Update Ad.js

--- models/Ad.js
+++ models/Ad.js
@@ -1,48 +1,12 @@
 const mongoose = require('mongoose');
 
 const AdSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true
-    },
-    url: {
-        type: String,
-        required: true
-    },
-    rewardAmount: { 
-        type: Number,
-        required: true,
-        min: 0.01 
-    },
-    viewDuration: {
-        type: Number,
-        required: true,
-        min: 5
-    },
-    totalViews: { 
-        type: Number,
-        required: true
-    },
-    remainingViews: { 
-        type: Number
-    },
-    isActive: {
-        type: Boolean,
-        default: true
-    },
-    // ربط الإعلان بالمعلن الذي قام بإنشائه (ميزة المعلنين)
-    createdBy: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Advertiser', 
-        required: false 
-    }
+    title: { type: String, required: true },
+    url: { type: String, required: true },
+    rewardAmount: { type: Number, required: true },
+    totalViews: { type: Number, required: true },
+    remainingViews: { type: Number, required: true },
+    isActive: { type: Boolean, default: true }
 }, { timestamps: true });
-
-AdSchema.pre('save', function(next) {
-    if (this.isNew) {
-        this.remainingViews = this.totalViews;
-    }
-    next();
-});
 
 module.exports = mongoose.model('Ad', AdSchema);
